382b854eb34e4f5e6d7c7a6e855296003292d3b7
Yognjai <dev.yongjai@gmail.com>
2017-09-27 21:37:31 +0900

    Create ARKit project

---
+++ ARKit_example/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ARKit_example
+//
+//  Created by YongJai on 27/09/2017.
+//  Copyright © 2017 YongJai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SceneKit/SceneKit.h>
+#import <ARKit/ARKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ ARKit_example/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ARKit_example
+//
+//  Created by YongJai on 27/09/2017.
+//  Copyright © 2017 YongJai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
